646999d5cfc88f588fd4dd694a06340e78661b2a
101space <101kingste@gmail.com>
2025-03-27 20:32:54 -0400

    Remove unused useThree import

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from 'react'
-import { useFrame, useThree } from '@react-three/fiber'
+import { useFrame } from '@react-three/fiber'
 import { OrbitControls, useGLTF, Environment, Float } from '@react-three/drei'
 import { Group } from 'three'
 import { MODEL_URL } from '../config/cdn'
