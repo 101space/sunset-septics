d363c5eff2871353d2236f729c9372d0592ce101
101space <101kingste@gmail.com>
2025-03-29 11:47:26 -0400

    Fix background color and model loading issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Scene } from './components/Scene'
 import { Gallery } from './components/Gallery'
 import { Reference } from './components/Reference'
