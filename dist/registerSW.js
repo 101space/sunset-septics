dc19c6bc6001467c863b243f11ee5de88365ea1d
101space <101kingste@gmail.com>
2025-03-31 19:10:58 -0400

    Rebuild and update dist directory

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sunset-septics/sw.js', { scope: '/sunset-septics/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
--- h
+++
@@ -1,5 +0,0 @@
-  gh-pages[m
-* [32mgithub-pages-branch[m
-  main[m
-  [31mremotes/origin/github-pages-branch[m
-  [31mremotes/origin/main[m
